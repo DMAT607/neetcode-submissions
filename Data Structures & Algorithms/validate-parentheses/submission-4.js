9cbcf6ece358e8e3a48fec4d4713621d1643bc5d
DMAT607 <67300271+DMAT607@users.noreply.github.com>
2026-08-05 12:19:21 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        };
+        const stack = [];
+        for(let i = 0; i < s.length; i++) {
+            if(pairs[s[i]]) {
+                const recent = stack.pop();
+                if(recent !== pairs[s[i]]) return false;
+                continue;
+            }
+            stack.push(s[i])
+        }
+
+        return stack.length === 0 ? true : false;
+    }
+}

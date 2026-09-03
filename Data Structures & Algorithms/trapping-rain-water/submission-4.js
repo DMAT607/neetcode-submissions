a7c599fd05b50e8275d80e5d7f6213c790e30e03
DMAT607 <67300271+DMAT607@users.noreply.github.com>
2026-09-03 12:10:22 +0530

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let totalWater = 0;
+        let leftMax = 0;
+        let rightMax = 0;
+
+        let left = 0;
+        let right = height.length - 1;
+
+        while(left < right) {
+            
+            if(height[left] <= height[right]) {
+                if(height[left] < leftMax) {
+                    totalWater += leftMax - height[left]
+                } else {
+                    leftMax = height[left]
+                }
+                left++;
+            } else {
+                if(height[right] < rightMax) {
+                    totalWater += rightMax - height[right]
+                } else {
+                    rightMax = height[right]
+                }
+                right--;
+            }
+        }
+        return totalWater;
+    }
+}
